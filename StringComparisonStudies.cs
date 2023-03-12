0e7c14249761dd36c464efe26912e32592e86bc8
viniciusscorza <vinicius.scorza@hotmail.com>
2023-03-12 01:39:51 -0300

    Update

--- StringComparisonStudies.cs
+++ StringComparisonStudies.cs
@@ -21,10 +21,18 @@ namespace Strings
             // Contains() is a case sensitive method than returns an boolean, and may be more useful in most cases
             Console.WriteLine(doom.Contains("Valve")); // return false
             Console.WriteLine(doom.Contains("John Romero s2")); // return true
+
             //The case sensitive property may be ignored
+            // NOTE: System.StringComparison enumerator specifies the culture, case and sort rules to be used by certain overloads of the Compare() and Equals() methods.
             Console.WriteLine(doom.Contains("john romero S2", StringComparison.OrdinalIgnoreCase));
 
-
+            // StartsWith() verify if the string starts with determinate text
+            Console.WriteLine(divinity.StartsWith("La")); // return true
+            Console.WriteLine(doom.StartsWith("Romero")); // return false
+            // EndsWith() do the same, but to the end
+            Console.WriteLine(counterStrike.EndsWith("e", StringComparison.OrdinalIgnoreCase)); // return true
+            Console.WriteLine(doom.EndsWith(")", StringComparison.OrdinalIgnoreCase)); // return true
+            Console.WriteLine(halfLife.EndsWith("v")); // return false
         }
     }
 }
